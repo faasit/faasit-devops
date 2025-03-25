673c30e9d751d70c3966a4193f15b3fa0e027bca
Shenghao Wu <wushenghaop@qq.com>
2025-03-26 00:13:50 +0800

    fix: batch-test.sh

--- batch-test.sh
+++ batch-test.sh
@@ -6,11 +6,17 @@ CURRENT_TIME=$(TZ=Asia/Shanghai date +"%Y-%m-%d %H:%M:%S UTC+8")
 sed -i "s/# Modified on .*/# Modified on ${CURRENT_TIME}/" "$FILE"
 echo "TIME: ${CURRENT_TIME}"
 
+# commit code
+git add "$FILE"
+git commit -m "test on: ${CURRENT_TIME}"
+
 # push branches to trigger CI/CD
 BRANCHES=("master" "aliyun" "knative" "multi-env")
 
 for branch in "${BRANCHES[@]}"; do
   git checkout -B "$branch"
-  git push --force-with-lease origin "$branch"  # 推送分支
+  git push --force-with-lease origin "$branch"
   echo "Push branch [$branch] and trigger CI/CD."
 done
+
+git checkout master
