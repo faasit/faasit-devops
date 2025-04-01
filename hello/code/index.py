69270142c428fd2dcbeece0d3c874e6d0ea661d8
Shenghao Wu <wushenghaop@qq.com>
2025-04-01 19:17:50 +0800

    Set isCanary to True for aliyun-canary on: 2025-04-01 19:17:42 UTC+8

--- hello/code/index.py
+++ hello/code/index.py
@@ -1,11 +1,11 @@
 from faasit_runtime import function, FaasitRuntime
 
-# Modified on 2025-04-01 19:08:23 UTC+8
+# Modified on 2025-04-01 19:17:42 UTC+8
 @function
 def hello(rt: FaasitRuntime):
     return rt.output({
         "input": rt.input(),
-        "isCanary": False
+        "isCanary": True
     })
 
 hello = hello.export()
