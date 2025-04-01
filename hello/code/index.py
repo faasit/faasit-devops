b87e297ba22308d5f760893e1ccbbdd557116f94
Shenghao Wu <wushenghaop@qq.com>
2025-04-01 19:08:44 +0800

    isCanary: False

--- hello/code/index.py
+++ hello/code/index.py
@@ -1,11 +1,11 @@
 from faasit_runtime import function, FaasitRuntime
 
-# Modified on 2025-04-01 16:04:07 UTC+8
+# Modified on 2025-04-01 19:08:23 UTC+8
 @function
 def hello(rt: FaasitRuntime):
     return rt.output({
         "input": rt.input(),
-        "isCanary": True
+        "isCanary": False
     })
 
 hello = hello.export()
