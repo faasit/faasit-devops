444f2cc6032077e0cc1a02309e9cce410f862523
Shenghao Wu <wushenghaop@qq.com>
2025-04-01 13:33:57 +0800

    test on: 2025-04-01 13:33:57 UTC+8

--- hello/code/index.py
+++ hello/code/index.py
@@ -1,6 +1,6 @@
 from faasit_runtime import function, FaasitRuntime
 
-# Modified on 2025-03-31 17:11:49 UTC+8
+# Modified on 2025-04-01 13:33:57 UTC+8
 @function
 def hello(rt: FaasitRuntime):
     return rt.output(rt.input())
