861ad7b3ac9e8d9bd19e1d028237b2bb9072f66e
Shenghao Wu <wushenghaop@qq.com>
2025-04-01 19:07:28 +0800

    feat: 19:07

--- hello/code/index.py
+++ hello/code/index.py
@@ -3,7 +3,9 @@ from faasit_runtime import function, FaasitRuntime
 # Modified on 2025-04-01 16:04:07 UTC+8
 @function
 def hello(rt: FaasitRuntime):
-    rt.input()["isCanary"] = False
-    return rt.output(rt.input())
+    return rt.output({
+        "input": rt.input(),
+        "isCanary": True
+    })
 
 hello = hello.export()
