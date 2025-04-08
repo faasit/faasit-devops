159d68b2e89bb7dcdee51eb2560017b2a6e7ed5f
Shenghao Wu <wushenghaop@qq.com>
2025-04-08 13:01:06 +0800

    test on: 2025-04-08 13:01:06 UTC+8

--- hello/code/index.py
+++ hello/code/index.py
@@ -3,9 +3,9 @@ from faasit_runtime import function, FaasitRuntime
 @function
 def hello(rt: FaasitRuntime):
     return rt.output({
-        "deploy_time": "2025-04-08 11:46:18 UTC+8",
+        "deploy_time": "2025-04-08 13:01:06 UTC+8",
         "input": rt.input(),
-        "isCanary": True
+        "isCanary": False
     })
 
 hello = hello.export()
