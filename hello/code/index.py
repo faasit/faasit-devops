62c0dcd9cf66a0998e5fcb4610288813355f63a8
xdydy <dydy@i2ec.top>
2025-04-15 08:58:59 +0800

    test on: 2025-04-15 08:58:32 UTC+8

--- hello/code/index.py
+++ hello/code/index.py
@@ -3,7 +3,7 @@ from faasit_runtime import function, FaasitRuntime
 @function
 def hello(rt: FaasitRuntime):
     return rt.output({
-        "deploy_time": "2025-04-09 20:31:29 UTC+8",
+        "deploy_time": "2025-04-15 08:58:32 UTC+8",
         "input": rt.input(),
         "isCanary": False
     })
