5aab2a11edf41a1b06778204d5aba14696f81d26
Xdydy <dydy@i2ec.top>
2025-08-13 10:24:31 +0800

    Set isCanary to True on: 2025-08-13 10:22:53 UTC+8

--- hello/code/index.py
+++ hello/code/index.py
@@ -5,7 +5,7 @@ def hello(rt: FaasitRuntime):
     return rt.output({
         "deploy_time": "2025-08-13 10:22:53 UTC+8",
         "input": rt.input(),
-        "isCanary": False
+        "isCanary": True
     })
 
 hello = hello.export()
